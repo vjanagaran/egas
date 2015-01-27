9ff931120ab213f71c836d16250c759e62ad2006
programmer <programmer@programmer-PC>
2015-01-27 12:44:45 +0530

    Fixed alignment issues

--- js/index.js
+++ js/index.js
@@ -18,7 +18,6 @@ var router = new $.mobile.Router([{
         "#shopping": {handler: "shoppingPage", events: "bs"},
         "#shoppingitems(?:[?/](.*))?": {handler: "shoppingitemsPage", events: "bs"},
         "#cart": {handler: "cartPage", events: "bs"},
-        "#delivery": {handler: "deliveryPage", events: "bs"},
         "#payment": {handler: "paymentPage", events: "bs"},
         "#me": {handler: "mePage", events: "bs"},
         "#orders": {handler: "ordersPage", events: "bs"},
@@ -45,6 +44,7 @@ var router = new $.mobile.Router([{
             },
             paymentPage: function (type, match, ui) {
                 log("Payment Items page", 3);
+                $("#cash_pay").attr("checked", true);
                 $("#payment_items_total").html(grand_total);
             },
             morePage: function (type, match, ui) {
@@ -132,12 +132,12 @@ function log(msg, level) {
 }
 
 
-/********  Common Functions and Variables **/
+/********  Common Functions and Global Variables **/
 
 var loading = '<div class="align-center"><br/><br/><img src="img/loading.gif" width="60" /></div>';
 var cart = {items: [], decs: "", delivery: ""};
-var confirm_id = 0;
 var grand_total = 0;
+
 function calcCart() {
     var cart_qty = 0;
     $.each(cart.items, function (index, row) {
@@ -214,16 +214,22 @@ function loadShoppingItems(cat) {
 }
 
 function addToCart(id) {
+    $('#request_process').attr('onclick', 'addConfirmed(' + id + ')');
     var qty = $("#item_qty_" + id).val();
     var name = $("#item_name_" + id).html();
-    confirm_id = id;
-    $("#confirm_text").html("You're adding <b>" + name + "</b> into cart <b>" + qty + " nos.</b>");
+    var act = "adding";
+    var title = "Add";
+    if ($("#menu_item_" + id).hasClass("selected")) {
+        act = "updating";
+        title = "Update";
+    }
+    $("#confirm_title").html(title + " Item");
+    $("#confirm_text").html("You're " + act + " <b>" + name + "</b> into cart <b>" + qty + " nos.</b>");
     $("#popupDialog").popup("open");
 }
 
-function addConfirmed() {
+function addConfirmed(id) {
     $("#popupDialog").popup("close");
-    var id = confirm_id;
     var qty = $("#item_qty_" + id).val();
     var rate = $("#item_price_" + id).html();
     var name = $("#item_name_" + id).html();
@@ -275,6 +281,15 @@ function decreaseQty(id) {
 }
 
 function removeFromCart(id) {
+    $('#request_process').attr('onclick', 'removeConfirmed(' + id + ')');
+    var name = $("#item_name_" + id).html();
+    $("#confirm_title").html("Remove Item");
+    $("#confirm_text").html("You're removing <b>" + name + "</b> from cart");
+    $("#popupDialog").popup("open");
+}
+
+function removeConfirmed(id) {
+    $("#popupDialog").popup("close");
     $.each(cart.items, function (index, row) {
         if (row.id == id) {
             cart.items.splice(index, 1);
@@ -299,11 +314,11 @@ function showMyCart() {
     var g_total = 0;
     if (cart.items.length > 0) {
         $("#cart div[data-role=footer]").removeClass("remove-item");
-        out = out + '<table data-role="table" data-mode="none"><thead><tr><th class="align-left">Your Order</th><th class="align-right">Qty</th><th class="align-right">Amount</th><th>&nbsp;</th><th>&nbsp;</th></tr></thead><tbody>';
+        out = out + '<table data-role="table" data-mode="none"><thead><tr><th class="align-left">Your Order</th><th class="align-right">Qty</th><th class="align-right">Amount</th><th>Manipulate</th></tr></thead><tbody>';
         $.each(cart.items, function (index, row) {
             out = out + '<tr><td class = "align-left">' + row.name + '</td><td class="align-right"><div class="select-qty"><a onclick="decreaseCartQty(' + row.id +
-                    ')">-</a> <input data-role="none" name="qty" type="text" readonly="true" id="cart_item_' + row.id + '" value="' + row.qty + '"> <a onclick="increaseCartQty(' + row.id
-                    + ')">+</a></div></td><td class="align-right">' + (parseInt(row.rate) * parseInt(row.qty)).toFixed(2) + '</td><td ><a class="symbol" onclick="updateCart(' + row.id +
+                    ')">&ndash;</a> <input data-role="none" name="qty" type="text" readonly="true" id="cart_item_' + row.id + '" value="' + row.qty + '"> <a onclick="increaseCartQty(' + row.id
+                    + ')">+</a></div></td><td class="align-right">' + (parseInt(row.rate) * parseInt(row.qty)).toFixed(2) + '</td><td class="align-center"><a class="symbol" onclick="updateCart(' + row.id +
                     ')">&#10004;</a> <a class="symbol" onclick="removeItem(' + row.id + ');">&#10008;</a></td></tr>';
             total = total + parseFloat(row.rate) * parseInt(row.qty);
             if (isNaN(cart_tax[row.tax])) {
@@ -314,14 +329,14 @@ function showMyCart() {
         g_total = total;
         $.each(cart_tax, function (index, val) {
             tax_row = tax_row + '<tr><td colspan="2" class="align-left">TAX ' + index + '%</td><td class="align-right">' + val.toFixed(2) +
-                    '</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
+                    '</td><td>&nbsp;</td></tr>';
             g_total = g_total + val;
         });
-        out = out + '<tr><td colspan="3">&nbsp;</td></tr>';
-        out = out + '<tr><td class="align-left">Total</td><td class="align-right" colspan="2">' + total.toFixed(2) + '</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
+        out = out + '<tr><td colspan="4">&nbsp;</td></tr>';
+        out = out + '<tr><td colspan="2" class="align-left">Total</td><td class="align-right">' + total.toFixed(2) + '</td><td>&nbsp;</td></tr>';
         out = out + tax_row;
-        out = out + '<tr><td colspan="2" class="align-left">Grand Total</td><td class="align-right">' + g_total.toFixed(2) + '</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
-        out = out + '<tr><td colspan="5"><textarea name="orderdecs" id="orderdecs" placeholder="Order description (optional)...."></textarea></td></tr></tbody></table>';
+        out = out + '<tr><td colspan="2" class="align-left">Grand Total</td><td class="align-right">' + g_total.toFixed(2) + '</td><td>&nbsp;</td></tr>';
+        out = out + '<tr><td colspan="4"><textarea name="orderdecs" id="orderdecs" placeholder="Order description (optional)...."></textarea></td></tr></tbody></table>';
     } else {
         out = "<p>No items found in your cart</p>";
         $("#cart div[data-role=footer]").addClass("remove-item");
@@ -347,6 +362,22 @@ function decreaseCartQty(id) {
 }
 
 function updateCart(id) {
+    $('#cart_request_process').attr('onclick', 'updateCartConfirmed(' + id + ')');
+    var qty = $("#cart_item_" + id).val();
+    var name = "";
+    $.each(cart.items, function (index, row) {
+        if (row.id == id) {
+            name = row.name;
+            qty = row.qty;
+        }
+    });
+    $("#cart_confirm_title").html("Update Item");
+    $("#cart_confirm_text").html("You're updating <b>" + name + "</b> into cart <b>" + qty + " nos.</b>");
+    $("#cart_manipulation").popup("open");
+}
+
+function updateCartConfirmed(id) {
+    $("#cart_manipulation").popup("close");
     var new_qty = $("#cart_item_" + id).val();
     $.each(cart.items, function (index, row) {
         if (row.id == id) {
@@ -359,14 +390,98 @@ function updateCart(id) {
     $("#cart_items_total").html(grand_total);
 }
 
+function removeItem(id) {
+    var name = "";
+    $('#cart_request_process').attr('onclick', 'removeItemConfirmed(' + id + ')');
+    $.each(cart.items, function (index, row) {
+        if (row.id == id) {
+            name = row.name;
+        }
+    });
+    $("#cart_confirm_title").html("Remove Item");
+    $("#cart_confirm_text").html("You're removing <b>" + name + "</b> from cart");
+    $("#cart_manipulation").popup("open");
+}
+
+function removeItemConfirmed(id) {
+    $("#cart_manipulation").popup("close");
+    $.each(cart.items, function (index, row) {
+        if (row.id == id) {
+            cart.items.splice(index, 1);
+            return false;
+        }
+    });
+    $("#menu_item_" + id).removeClass("selected");
+    showMyCart();
+    $("#cart_items_total").html(grand_total);
+}
+
 function processStep1() {
-    /*var decs = $("#orderdecs").val();
-     cart.decs = decs;
-     if (getVal(config.user_id) != null) {
-     $(":mobile-pagecontainer").pagecontainer("change", "#payment");
-     } else {
-     $(":mobile-pagecontainer").pagecontainer("change", "#registration");
-     }*/
+    var decs = $("#orderdecs").val();
+    cart.decs = decs;
     $(":mobile-pagecontainer").pagecontainer("change", "#payment");
 }
 
+
+/****** Payment page functions  ***/
+
+function processOrder() {
+    var id = getVal(config.user_id);
+    var delivery = cart.delivery;
+    var decs = cart.decs;
+    if ($("input[name='cash_pay']").prop('checked') == true) {
+        var data = {items: []};
+        var items = [];
+        $.each(cart.items, function (index, row) {
+            var item = {
+                item_id: row.id,
+                quantity: row.qty
+            };
+            items.push(item);
+        });
+        data = {
+            items: items,
+            user: id,
+            notes: decs,
+            delivery: delivery,
+            total: grand_total,
+            address1: getVal(config.user_address1),
+            address2: getVal(config.user_address2),
+            area: getVal(config.user_area),
+            city: getVal(config.user_city),
+            pincode: getVal(config.user_pincode),
+            alternetno: getVal(config.user_alternet_number)
+        };
+        $.ajax({
+            type: "POST",
+            url: config.api_url + "module=order&action=create",
+            data: data,
+            cache: false,
+            success: function (html) {
+                if (html.error == false) {
+                    cart.items = [];
+                    grand_total = 0;
+                    $("#order_success .ui-content a").removeAttr("data-rel");
+                    $("#order_success .ui-content a").attr("onclick", "redirectOrdersPage()");
+                    $("#order_success_text").html("<b>" + html.message + "</b>");
+                    $("#order_success").popup("open");
+                } else {
+                    $("#order_success_text").html("<b>" + html.message + "</b>");
+                    $("#order_success .ui-content a").removeAttr("onclick");
+                    $("#order_success .ui-content a").attr("data-rel", "back");
+                    $("#order_success").popup("open");
+                }
+            },
+            error: function (request, status, error) {
+                $("#success_msg").empty();
+                $("#success_msg").append("Process not successfull try again later......");
+            }
+        });
+    } else {
+        $("#success_msg").append("<b>Please select the payment mode</b>");
+    }
+}
+
+function redirectOrdersPage() {
+    $(":mobile-pagecontainer").pagecontainer("change", "#orders");
+}
